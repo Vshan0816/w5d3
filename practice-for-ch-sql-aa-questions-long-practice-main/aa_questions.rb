b17d8d29a52f4e0b2890b7254f054292842842c5
Sean <pca.sean.jeun@gmail.com>
2023-07-26 16:38:34 -0400

    User class method built/compelete

--- practice-for-ch-sql-aa-questions-long-practice-main/aa_questions.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/aa_questions.rb
@@ -10,3 +10,43 @@ class QuestionsDatabase < SQLite3::Database
     end
     
 end
+
+class User
+    def initialize(hash)
+        @id = hash["id"]
+        @fname = hash["fname"]
+        @lname = hash["lname"]
+    end
+    def self.find_by_id(prime_key)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM users WHERE id = #{prime_key}")
+        User.new(data[0])
+    end
+end
+
+class Question
+    def self.find_by_id(prime_key)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM questions WHERE id = #{prime_key}")
+        Question.new(data[0])
+    end
+end
+
+class QuestionsFollow
+    def self.find_by_id(prime_key)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows WHERE id = #{prime_key}")
+        QuestionFollow.new(data[0])
+    end
+end
+
+class Reply
+    def self.find_by_id(prime_key)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM replies WHERE id = #{prime_key}")
+        Reply.new(data[0])
+    end
+end
+
+class QuestionLike
+    def self.find_by_id(prime_key)
+        data = QuestionsDatabase.instance.execute("SELECT * FROM question_likes WHERE id = #{prime_key}")
+        QuestionLike.new(data[0])
+    end
+end
